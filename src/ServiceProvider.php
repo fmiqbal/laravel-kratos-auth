47691481b174dd14918db3ce542400695c676500
Fikri Muhammad Iqbal <fikri.miqbal23@gmail.com>
2025-03-11 11:11:02 +0700

    refactor: move Ory initialization to own method

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Fmiqbal\KratosAuth;
 
+use Closure;
+use GuzzleHttp;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use InvalidArgumentException;
 use Ory;
 
 class ServiceProvider extends LaravelServiceProvider
@@ -21,21 +24,53 @@ class ServiceProvider extends LaravelServiceProvider
             __DIR__ . '/../config/kratos.php' => config_path('kratos.php'),
         ]);
 
+        $this->configureOry();
         $this->configureGuard();
     }
 
-    protected function configureGuard()
+    protected function configureOry(): void
     {
-        $ory = (new Ory\Client\Configuration())
-            ->setHost(config('kratos.admin_url'))
+        $config = (new Ory\Client\Configuration())
+            ->setHost(config('kratos.url'))
             ->setDebug(config('kratos.debug'));
 
-        $this->app['auth']->extend('kratos', function (Application $app) use ($ory) {
-            $guard = new KratosGuard($app['request'], $ory);
+        $this->app->singleton(Ory\Client\Api\FrontendApi::class, function () use ($config) {
+            return new Ory\Client\Api\FrontendApi(
+                $this->getClient(),
+                $config
+            );
+        });
+    }
+
+    protected function configureGuard(): void
+    {
+        $this->app['auth']->extend('kratos', function (Application $app) {
+            $frontendApi = $app->make(Ory\Client\Api\FrontendApi::class);
+
+            $guard = new KratosGuard($app['request'], $frontendApi);
 
             $app->refresh('request', $guard, 'setRequest');
 
             return $guard;
         });
     }
+
+    /**
+     * @return GuzzleHttp\Client
+     */
+    protected function getClient(): GuzzleHttp\Client
+    {
+        $client = config('kratos.guzzle_client');
+
+        if ($client instanceof Closure) {
+            $client = $client();
+        }
+
+        if (! $client instanceof GuzzleHttp\Client) {
+            throw new InvalidArgumentException("config('kratos.guzzle-client') should be instance of GuzzleHttp\\Client");
+        }
+
+        return $client;
+    }
+
 }
